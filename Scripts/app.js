f40f735d95819f93b89eece58b85998268b91551
logan morris <logan-morr@hotmail.ca>
2022-01-21 16:32:47 -0600

    UPDATED ICE 2

--- Scripts/app.js
+++ Scripts/app.js
@@ -9,6 +9,43 @@
         {
             location.href = "about.html";
         });
+
+        //step 1 - get a referance to an entry point(s) (insertion/deletion point)
+        let MainContent = document.getElementsByTagName("main")[0];
+        let documentBody = document.body;
+
+        //step 2 - Create a html element in memory
+        let MainParagraph = document.createElement("p");
+        let article = document.createElement("article");
+        let articleParagraph = `<pid="articleParagraph" class="mt-3">this is the article paragragh</p>`;
+
+        //3 configure new element
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class","mt-3");
+        let firstString = "this is";
+        let secondString = `${firstString} the main paragraph`;
+        MainParagraph.textContent = secondString;
+        article.setAttribute("class","container");
+
+        
+        //4 perform insertion / deletion
+
+        //example of insert after (append)
+        MainContent.appendChild(MainParagraph);
+        article.innerHTML = articleParagraph;
+        documentBody.appendChild(article);
+        
+
+        //example of insert before
+        //MainContent.before(MainParagraph);
+
+        //example of deletion
+        //document.getElementById("AboutUsButton").remove();
+        //aboutUsButton.remove();
+
+        //ES6 and HTML5 => Template strings => 
+
+
     }
 
     //names function
@@ -22,8 +59,7 @@
                 DisplayHome();
                 break;
         }
-        let aboutUsButton = document.getElementById("AboutUsButton");
-        console.log(aboutUsButton);
+        
     }
 
 
